5878e8e19c375ab970d189bbb4abe91909518786
Raony Fernandes dos Reis <75029692+raonygamer@users.noreply.github.com>
2026-08-15 02:26:22 -0300

    refactor: document application orchestration boundary

--- include/quartz/client/Application.hpp
+++ include/quartz/client/Application.hpp
@@ -4,6 +4,7 @@ namespace quartz::client
     class Application
     {
     public:
+        // Owns process-level orchestration; subsystem implementation lives behind the client module boundaries.
         int run(int argc, char* argv[]);
     };
 }
