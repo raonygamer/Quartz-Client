c9d093657396c0336808e54828c7a7a1c529287b
Raony Fernandes dos Reis <75029692+raonygamer@users.noreply.github.com>
2026-08-15 23:29:13 -0300

    feat: add configurable Quartz UI themes

---
+++ include/quartz/client/ui/Theme.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+namespace quartz::client
+{
+    struct VisualizerSettings;
+}
+
+namespace quartz::client::ui
+{
+    enum class Theme : int
+    {
+        QuartzCyan,
+        Graphite,
+        OceanBlue,
+        Emerald,
+        DevilukePink,
+        KurosakiPink,
+        YamiGolden,
+        KirisakiPurple,
+        Count
+    };
+
+    [[nodiscard]] const char* themeName(Theme theme) noexcept;
+    [[nodiscard]] bool suspiciousTheme(Theme theme) noexcept;
+    void applyTheme(Theme theme) noexcept;
+    void applyTheme(int theme) noexcept;
+    bool drawThemeSelector(VisualizerSettings& settings);
+}
