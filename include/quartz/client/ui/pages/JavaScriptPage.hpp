25f0cc991616aad82afaee4f658d753c639c246b
Raony Fernandes dos Reis <75029692+raonygamer@users.noreply.github.com>
2026-08-15 23:23:14 -0300

    refactor: move scripts into scripting navigation

--- include/quartz/client/ui/pages/JavaScriptPage.hpp
+++ include/quartz/client/ui/pages/JavaScriptPage.hpp
@@ -7,8 +7,8 @@ namespace quartz::client::ui
     {
     public:
         [[nodiscard]] std::string_view id() const noexcept override { return "javascript"; }
-        [[nodiscard]] std::string_view title() const noexcept override { return "JavaScript"; }
-        [[nodiscard]] PageSection section() const noexcept override { return PageSection::Runtime; }
+        [[nodiscard]] std::string_view title() const noexcept override { return "Scripts"; }
+        [[nodiscard]] PageSection section() const noexcept override { return PageSection::Scripting; }
         void render(PageContext& context, PageManager& manager) override;
     };
 }
