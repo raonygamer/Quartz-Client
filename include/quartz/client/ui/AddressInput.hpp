f1a15e5091a660bdda7189764c679adeaa9a0190
Raony Fernandes dos Reis <75029692+raonygamer@users.noreply.github.com>
2026-08-16 03:37:23 -0300

    feat: allow address input behavior flags

--- include/quartz/client/ui/AddressInput.hpp
+++ include/quartz/client/ui/AddressInput.hpp
@@ -2,11 +2,12 @@
 #include "quartz/client/Forward.hpp"
 #include <cstddef>
 #include <cstdint>
+#include <imgui.h>
 #include <string>
 #include <string_view>
 
 namespace quartz::client::ui
 {
     bool evaluateAddressExpression(pid_t pid, std::string_view expression, std::uintptr_t& value, std::string& error);
-    bool drawAddressInput(const char* label, char* buffer, std::size_t bufferSize, pid_t pid, float width = 0.0f);
+    bool drawAddressInput(const char* label, char* buffer, std::size_t bufferSize, pid_t pid, float width = 0.0f, ImGuiInputTextFlags flags = 0);
 }
