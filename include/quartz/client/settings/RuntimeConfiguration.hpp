14284e1d33a123a1203920fcc92e0c052147acee
Raony Fernandes dos Reis <75029692+raonygamer@users.noreply.github.com>
2026-08-16 04:55:13 -0300

    feat: add reverse engineering configuration

--- include/quartz/client/settings/RuntimeConfiguration.hpp
+++ include/quartz/client/settings/RuntimeConfiguration.hpp
@@ -6,6 +6,15 @@ namespace quartz::client
     struct RuntimeConfiguration
     {
         std::size_t SignatureScanChunkBytes = 4ULL * 1024ULL * 1024ULL;
+        float DisassemblyRefreshHz = 0.5f;
+        float RawBytesRefreshHz = 0.25f;
+        bool AssemblerFillNops = true;
+        bool AssemblerWholeInstructions = true;
+        bool AssemblerConsumeFollowing = false;
+        bool AssemblerVerifyBeforeWrite = true;
+        bool AssemblerRequireConfirmation = true;
+        bool AssemblerAutoRedisassemble = true;
+        bool AssemblerKeepOriginalBytes = true;
     };
 
     RuntimeConfiguration& runtimeConfiguration() noexcept;
