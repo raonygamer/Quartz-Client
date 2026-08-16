2a398338f8593ebb299d286dd6eaaa45211a06bc
Raony Fernandes dos Reis <75029692+raonygamer@users.noreply.github.com>
2026-08-15 23:25:40 -0300

    feat: add reverse engineering signature maker interface

---
+++ include/quartz/client/ui/SignatureMaker.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <cstdint>
+#include <sys/types.h>
+
+namespace quartz::client::ui
+{
+    struct PageContext;
+    class PageManager;
+
+    void requestSignatureMaker(pid_t pid, std::uintptr_t address) noexcept;
+    bool signatureMakerWantsFocus() noexcept;
+    void drawSignatureMaker(PageContext& context, PageManager& manager);
+}
