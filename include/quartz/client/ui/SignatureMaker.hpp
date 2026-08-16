24369e2ca657196a682ea4eee9576700ac313626
Raony Fernandes dos Reis <75029692+raonygamer@users.noreply.github.com>
2026-08-15 23:45:37 -0300

    feat: seed signatures from selected instruction ranges

--- include/quartz/client/ui/SignatureMaker.hpp
+++ include/quartz/client/ui/SignatureMaker.hpp
@@ -7,7 +7,7 @@ namespace quartz::client::ui
     struct PageContext;
     class PageManager;
 
-    void requestSignatureMaker(pid_t pid, std::uintptr_t address) noexcept;
+    void requestSignatureMaker(pid_t pid, std::uintptr_t address, int minimumInstructions = 1) noexcept;
     bool signatureMakerWantsFocus() noexcept;
     void drawSignatureMaker(PageContext& context, PageManager& manager);
 }
