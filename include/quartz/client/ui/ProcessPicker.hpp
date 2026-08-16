fcc6cb3f08136c7d4266b1c6eda17915449cbaf1
Raony Fernandes dos Reis <75029692+raonygamer@users.noreply.github.com>
2026-08-16 06:31:14 -0300

    feat: link reverse engineering process pickers

--- include/quartz/client/ui/ProcessPicker.hpp
+++ include/quartz/client/ui/ProcessPicker.hpp
@@ -6,4 +6,6 @@
 namespace quartz::client::ui
 {
     bool drawProcessPicker(const char* id, std::vector<RuntimeProcessInfo>& processes, pid_t& pid, char* search, std::size_t searchSize, float comboWidth = 420.0f);
+    pid_t sharedReverseEngineeringProcess() noexcept;
+    void setSharedReverseEngineeringProcess(pid_t pid) noexcept;
 }
