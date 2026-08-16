c520f9df482587d1139365032ed48eaeff2a6519
Raony Fernandes dos Reis <75029692+raonygamer@users.noreply.github.com>
2026-08-16 06:29:03 -0300

    feat: make silhouette opacity configurable

--- include/quartz/client/ui/ThemeBackground.hpp
+++ include/quartz/client/ui/ThemeBackground.hpp
@@ -6,6 +6,6 @@ struct ImVec2;
 
 namespace quartz::client::ui
 {
-    void drawThemeBackground(Theme theme, ImDrawList* drawList, const ImVec2& min, const ImVec2& max) noexcept;
-    void drawThemeBackground(int theme, ImDrawList* drawList, const ImVec2& min, const ImVec2& max) noexcept;
+    void drawThemeBackground(Theme theme, ImDrawList* drawList, const ImVec2& min, const ImVec2& max, float opacity = 1.0f) noexcept;
+    void drawThemeBackground(int theme, ImDrawList* drawList, const ImVec2& min, const ImVec2& max, float opacity = 1.0f) noexcept;
 }
