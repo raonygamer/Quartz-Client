d7723e0f6bfefb85b6aa4f5c4aece52d6189d488
Raony Fernandes dos Reis <75029692+raonygamer@users.noreply.github.com>
2026-08-16 03:20:13 -0300

    feat: add shared searchable process picker

---
+++ include/quartz/client/ui/ProcessPicker.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include "quartz/client/runtime/RuntimeTypes.hpp"
+#include <cstddef>
+#include <vector>
+
+namespace quartz::client::ui
+{
+    bool drawProcessPicker(const char* id, std::vector<RuntimeProcessInfo>& processes, pid_t& pid, char* search, std::size_t searchSize, float comboWidth = 420.0f);
+}
