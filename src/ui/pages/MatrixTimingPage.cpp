be865f8c9d04b8ee86f7f3ed85f7b369c9c50cb1
Raony Fernandes dos Reis <75029692+raonygamer@users.noreply.github.com>
2026-08-16 05:19:01 -0300

    i18n: translate matrix timing wait state

--- src/ui/pages/MatrixTimingPage.cpp
+++ src/ui/pages/MatrixTimingPage.cpp
@@ -1,30 +1,12 @@
 #include "quartz/client/ui/pages/MatrixTimingPage.hpp"
 #include "quartz/client/ui/PageContext.hpp"
 #include "quartz/client/ui/PageManager.hpp"
+#include "quartz/client/ui/I18n.hpp"
 
 namespace quartz::client::ui
 {
     void MatrixTimingPage::render(PageContext& context, PageManager& manager)
     {
-        auto& deviceState = context.deviceState;
-        PerformanceSnapshot performance{};
-        MatrixTimingProbeResult<ActiveProbeRows> timingProbe{};
-        bool hasPerformance = false;
-        bool hasTimingProbe = false;
-        std::uint64_t receivedPackets = 0;
-        {
-            std::lock_guard lock(deviceState.Mutex);
-            performance = deviceState.Performance;
-            timingProbe = deviceState.TimingProbe;
-            hasPerformance = deviceState.HasPerformance;
-            hasTimingProbe = deviceState.HasTimingProbe;
-            receivedPackets = deviceState.ReceivedPackets;
-        }
-
-        (void)manager;
-        if (hasTimingProbe)
-            drawTimingProbe(timingProbe);
-        else
-            ImGui::TextDisabled("Waiting for MatrixTimingProbeResult...");
+        auto& deviceState=context.deviceState; MatrixTimingProbeResult<ActiveProbeRows> timingProbe{}; bool hasTimingProbe=false; { std::lock_guard lock(deviceState.Mutex); timingProbe=deviceState.TimingProbe; hasTimingProbe=deviceState.HasTimingProbe; } (void)manager; if (hasTimingProbe) drawTimingProbe(timingProbe); else ImGui::TextDisabled("%s",i18n::tr("matrix.waiting"));
     }
 }
