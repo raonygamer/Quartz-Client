be48e62e4e6198cae20a4755667c4196ee6795fd
Raony Fernandes dos Reis <75029692+raonygamer@users.noreply.github.com>
2026-08-16 17:23:11 -0300

    refactor: allow focused i18n extension catalogs

--- include/quartz/client/ui/I18n.hpp
+++ include/quartz/client/ui/I18n.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <cstddef>
 #include <string_view>
 
 namespace quartz::client::ui::i18n
@@ -13,6 +14,11 @@ namespace quartz::client::ui::i18n
     [[nodiscard]] Language language() noexcept;
     [[nodiscard]] const char* languageName(Language language) noexcept;
     [[nodiscard]] const char* tr(std::string_view key) noexcept;
+    [[nodiscard]] const char* trExtension(std::string_view key) noexcept;
+    template<std::size_t N> [[nodiscard]] const char* tr(const char (&key)[N]) noexcept
+    {
+        const std::string_view view(key,N-1); if (const char* translated=trExtension(view)) return translated; return tr(view);
+    }
     void setLanguage(Language language) noexcept;
     void loadLanguagePreference() noexcept;
     void saveLanguagePreference() noexcept;
